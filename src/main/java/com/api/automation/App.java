71feb41aa8ba1f8d7deb057fccbba12b3afa3a3c
Karthick Srinivasan <92621517+angrybuddha24@users.noreply.github.com>
2021-12-30 09:58:46 +0530

    removed unused imports

--- src/main/java/com/api/automation/App.java
+++ src/main/java/com/api/automation/App.java
@@ -1,11 +1,8 @@
 package com.api.automation;
 
-import com.api.automation.practice.Slf4jDemo;
-
 public class App {
   public static void main(String[] args) {
-    // BasicConfigurator.configure();
-    new Slf4jDemo().slf4jPractice();
-
+    int a = 2;
+    System.out.println(a);
   }
 }
--- src/main/java/com/api/automation/practice/Slf4jDemo.java
+++ src/main/java/com/api/automation/practice/Slf4jDemo.java
@@ -2,8 +2,6 @@ package com.api.automation.practice;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.Marker;
-import org.slf4j.MarkerFactory;
 
 public class Slf4jDemo {
 
