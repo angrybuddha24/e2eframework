96d4286ee8a3be8acdbdf1447c130b2ffa7c516b
Karthick Srinivasan <92621517+angrybuddha24@users.noreply.github.com>
2022-01-11 10:31:58 +0530

    Lombok Intro

--- src/main/java/com/api/automation/App.java
+++ src/main/java/com/api/automation/App.java
@@ -1,8 +1,16 @@
 package com.api.automation;
 
+import com.api.automation.practice.LombokDemo;
+
 public class App {
   public static void main(String[] args) {
-    int a = 2;
-    System.out.println(a);
+    LombokDemo lombokDemo = new LombokDemo();
+    lombokDemo.setAge(26);
+    lombokDemo.setName("abc");
+    lombokDemo.setStudent(true);
+
+    System.out.println(lombokDemo.getAge());
+    System.out.println(lombokDemo.getName());
+    System.out.println(lombokDemo.isStudent());
   }
 }
---
+++ src/main/java/com/api/automation/practice/LombokDemo.java
@@ -0,0 +1,12 @@
+package com.api.automation.practice;
+
+import lombok.Data;
+
+
+public @Data class LombokDemo {
+    private String name;
+    private int age;
+    private boolean isStudent;
+
+
+}
