0131db79ea4ea225a76c50c955dd627997aa0ac0
Karthick Srinivasan <92621517+angrybuddha24@users.noreply.github.com>
2022-01-13 10:29:48 +0530

    lombok deep dive more annotations

--- src/main/java/com/api/automation/App.java
+++ src/main/java/com/api/automation/App.java
@@ -1,13 +1,17 @@
 package com.api.automation;
 
-import com.api.automation.practice.POJOtoJSON;
+import com.api.automation.practice.LombokDemo;
 
 public class App {
   public static void main(String[] args) {
 
 // new POJOtoJSON().convertPOJOtoJSONUsingGSON();
-new POJOtoJSON().convertPOJOtoJSONUsingJackson();
+// new POJOtoJSON().convertPOJOtoJSONUsingJackson();
+// System.out.println(new LombokDemo("aaa",20,true).toString());
+// System.out.println(new LombokDemo("aaa",20,true).hashCode());
+// System.out.println(new LombokDemo("aaa",20,true).equals(new LombokDemo("aaa",21,true)));
 
+System.out.println(new LombokDemo().getName());
 
 
 
--- src/main/java/com/api/automation/practice/POJOtoJSON.java
+++ src/main/java/com/api/automation/practice/POJOtoJSON.java
@@ -13,17 +13,25 @@ public class POJOtoJSON {
         lombokDemo.setAge(26);
         lombokDemo.setStudent(true);
 
+        System.out.println("Name: " + lombokDemo.getName());
+        System.out.println("Age: " + lombokDemo.getAge());
+        System.out.println("isStudent: " + lombokDemo.isStudent());
+
         System.out.println(new Gson().toJson(lombokDemo));
 
-        /* When values are passed through both constructor and set menthod, the set method values takes preference and 
-        the output is {"name":"abc","age":26,"isStudent":true} 
-        
-        When values are passed only via constructor, the constructor values are considered and 
-        the output is {"name":"def","age":22,"isStudent":false} 
-        
-        When some null values are passed via constructor, gson excludes the key while forming the json and 
-        the output is {"age":22,"isStudent":false}
-        */
+        /*
+         * When values are passed through both constructor and set menthod, the set
+         * method values takes preference and
+         * the output is {"name":"abc","age":26,"isStudent":true}
+         * 
+         * When values are passed only via constructor, the constructor values are
+         * considered and
+         * the output is {"name":"def","age":22,"isStudent":false}
+         * 
+         * When some null values are passed via constructor, gson excludes the key while
+         * forming the json and
+         * the output is {"age":22,"isStudent":false}
+         */
 
     }
 
@@ -34,6 +42,10 @@ public class POJOtoJSON {
         lombokDemo.setStudent(false);
 
         try {
+            System.out.println("Name: " + lombokDemo.getName());
+            System.out.println("Age: " + lombokDemo.getAge());
+            System.out.println("isStudent: " + lombokDemo.isStudent());
+            
             System.out.println(new ObjectMapper().writeValueAsString(lombokDemo));
         } catch (JsonProcessingException e) {
             e.printStackTrace();
@@ -42,6 +54,9 @@ public class POJOtoJSON {
 
     // output is {"name":"xyz","age":23,"student":false}
 
-    /* When some null values are passed via constructor, jackson includes the key while forming the json and sets the value as null and 
-        the output is {"name":null,"age":27,"student":true} */
+    /*
+     * When some null values are passed via constructor, jackson includes the key
+     * while forming the json and sets the value as null and
+     * the output is {"name":null,"age":27,"student":true}
+     */
 }
